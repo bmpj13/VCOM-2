2c91b97d26f96c38db2ae8ce76408677e5ca2d51
Cláudia Rodrigues <claudiacatarina232@gmail.com>
2020-01-02 12:27:36 +0000

    updated plot

--- train_pred.py
+++ train_pred.py
@@ -3,24 +3,22 @@ import numpy as np
 import pandas as pd
 import argparse
 import keras
-from keras.models import Model
 from models.unet3d import get_unet
- 
 import matplotlib.pyplot as plt
-import cv2 as cv
+ 
 
 # file to predict
-file = 'LNDb-0164_finding1_rad1.npy'
+file = 'LNDb-0001_finding1_rad3.npy'
 
 
 MASK_CUBES_PATH = 'lndb/mask_cubes/'
 SCAN_CUBES_PATH = 'lndb/scan_cubes/' 
-weightsPath = 'weights/3D-Unet.h5'
-logsPath = 'logs/3D-Unet.txt'
+WEIGHTS_PATH = 'weights/3D-Unet.h5'
+ 
 
 # get model
 model = get_unet()
-model.load_weights(weightsPath)
+model.load_weights(WEIGHTS_PATH)
  
 # original 
 org = np.load(SCAN_CUBES_PATH+file)
@@ -33,35 +31,36 @@ org /= 255.
 
 # input
 img = org.copy()
-img.shape = img.shape + (1,)
+img.shape =  (1,) + img.shape 
 img = img[None, :]
 
 img_mask_test = model.predict(img, batch_size=1, verbose=1)
 img_mask_test = np.squeeze(img_mask_test)
 img_mask_test = img_mask_test.astype(np.float64)
     
+
 # output
 img_mask_test[img_mask_test>=0.5] = 1
 img_mask_test[img_mask_test<0.5] = 0
  
-# print(img_mask_test)
 
 # original mask
 img_mask = np.load(MASK_CUBES_PATH+file)
     
+ 
+# plot 
 fig, axs = plt.subplots(3,3)
 axs[0,0].imshow(org[int(org.shape[0]/2),:,:])
-axs[1,0].imshow(img_mask[int(img_mask.shape[0]/2),:,:])
-axs[2,0].imshow(img_mask_test[int(img_mask_test.shape[0]/2),:,:])
-
-axs[0,1].imshow(org[:,int(org.shape[0]/2),:])
+axs[0,1].imshow(img_mask[int(img_mask.shape[0]/2),:,:])
+axs[0,2].imshow(img_mask_test[int(img_mask_test.shape[2]/2),:,:])
+ 
+axs[1,0].imshow(org[:,int(org.shape[0]/2),:])
 axs[1,1].imshow(img_mask[:,int(img_mask.shape[0]/2),:])
-axs[2,1].imshow(img_mask_test[:,int(img_mask_test.shape[0]/2),:])
+axs[1,2].imshow(img_mask_test[:,int(img_mask_test.shape[0]/2),:])
 
-axs[0,2].imshow(org[:,:,int(org.shape[0]/2)])
-axs[1,2].imshow(img_mask[:,:,int(img_mask.shape[0]/2)])
+axs[2,0].imshow(org[:,:,int(org.shape[0]/2)])
+axs[2,1].imshow(img_mask[:,:,int(img_mask.shape[0]/2)])
 axs[2,2].imshow(img_mask_test[:,:,int(img_mask_test.shape[0]/2)])
-
  
 
 plt.show()
